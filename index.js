778287b2bcbf0a9b3b0720abfc982c8987acef30
kimathinjoki <dfkimathinjoki@gmail.com>
2022-12-01 12:35:12 +0300

    Completed

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 // TODO: Listen for page loading (window)
+const print = (tag, value) =>{
+    console.log(tag,value);
+}
+
+
+window.onload = () =>{
+    console.log("Window:", "Loaded")
+}
 
 // TODO: Listen for page loading (document)
 
-// TODO: Access the h1 in HTML page
+document.addEventListener("DOMContentLoaded", (e)=> {
+    // console.log('event', e)
+    // TODO: Access the h1 in HTML page
+    const h1 = document.getElementById("app_title")
+    print(h1);
+    // TODO: Change color of app_title
+    h1.classList.add('title')
+    // TODO: Change color of app_paragraph
+    const p = document.getElementById("app_paragraph")
+    p.classList.add("paragraph")
+    // TODO: Change color of main page
+    const body = document.querySelector('body');
+    body.classList.add("main")
+    // TODO: Add a new element to the container
+    const div = document.getElementById('container');
+    const header = document.createElement('h2')
+    header.innerText = 'Newly created'
+    div.appendChild(header)
+    // TODO: Remove the h1 from the page
+    const b1 = document.getElementById('btn-title').addEventListener('click',()=> h1.remove())
+    const b2 = document.getElementById('btn-paragraph').addEventListener('click', ()=>{})
+    const b3 = document.getElementById('btn-main').addEventListener('click', ()=>{
+        div.remove()
+    })
 
-// TODO: Change color of app_title
+    document.addEventListener('mousedown', ()=> print('Mousedown', 'hello'))
 
-// TODO: Change color of app_paragraph
 
-// TODO: Change color of main page
 
-// TODO: Add a new element to the container
 
-// TODO: Remove the h1 from the page
+})
+
+
+
+
 
 // TODO: Listen for three mouse events
+
+function amClicked(){
+    alert('Feels nice to be clicked')
+}
+
+// const b1 = document.getElementById('btn-title').addEventListener('click',()=> h1.remove())
+// const b2 = document.getElementById('btn-paragraph').addEventListener('click', ()=>{})
+// const b3 = document.getElementById('btn-main').addEventListener('click', ()=>{
+//     div.remove()
+// })
+
